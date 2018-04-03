f943ac970cb972e316ad2cfc68bfed624f9da3ba
DeepCFoxXx <learncode23@gmail.com>
2018-04-03 16:47:30 +0100

    jet name passing

---
+++ Kaiju/src/main/java/JetJaguar.java
@@ -0,0 +1,7 @@
+public class JetJaguar extends Kaiju {
+
+    public JetJaguar(String name, int healthValue, int attackValue){
+        super(name, healthValue, attackValue);
+    }
+
+}
---
+++ Kaiju/src/test/java/TestJetJaguar.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestJetJaguar {
+
+    JetJaguar jetJaguar;
+
+    @Before
+    public void before() {
+        jetJaguar = new JetJaguar("Jet Jaguar", 300, 70);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jet Jaguar", jetJaguar.getName());
+    }
+
+}
