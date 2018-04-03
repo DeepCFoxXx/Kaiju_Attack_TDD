c3d69594609e6a8eb2ed48cd8245b3dc13ddbfce
DeepCFoxXx <learncode23@gmail.com>
2018-04-03 16:28:30 +0100

    first commit

---
+++ Kaiju/src/main/java/Godzilla.java
@@ -0,0 +1,7 @@
+public class Godzilla extends Kaiju {
+
+    public Godzilla(String name, int healthValue, int attackValue){
+        super(name, healthValue, attackValue);
+    }
+
+}
---
+++ Kaiju/src/test/java/TestGodzilla.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestGodzilla {
+
+    Godzilla godzilla;
+
+    @Before
+    public void before() {
+        godzilla = new Godzilla("Mecha Godzilla", 500, 100);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Mecha Godzilla", godzilla.getName());
+    }
+
+}
