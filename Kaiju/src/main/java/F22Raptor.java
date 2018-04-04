962fd04283d1e03e771898a29646acc0c8849d0e
DeepCFoxXx <learncode23@gmail.com>
2018-04-04 12:04:38 +0100

    f22 name passing

---
+++ Kaiju/src/main/java/F22Raptor.java
@@ -0,0 +1,7 @@
+public class F22Raptor extends Vehicle {
+
+    public F22Raptor(String name, int healthValue, int attackValue){
+        super(name, healthValue, attackValue);
+    }
+
+}
---
+++ Kaiju/src/test/java/TestF22Raptor.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestF22Raptor {
+
+    Godzilla godzilla;
+    F22Raptor f22Raptor;
+
+    @Before
+    public void before(){
+        godzilla = new Godzilla("1999 Godzilla", 400, 50);
+        f22Raptor = new F22Raptor("F22", 150, 10);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("F22", f22Raptor.getType());
+    }
+
+}
